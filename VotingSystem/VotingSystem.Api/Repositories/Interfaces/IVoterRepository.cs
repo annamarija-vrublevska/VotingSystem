69c7f29726ddfc796fd0e7ede13e17af6fad646e
Anna Marija Vrublevska <annamarija.vrublevska@gmail.com>
2022-06-26 18:25:58 +0300

    moved to other namespace

---
+++ VotingSystem/VotingSystem.Api/Repositories/Interfaces/IVoterRepository.cs
@@ -0,0 +1,14 @@
+﻿using VotingSystem.Api.Entities;
+
+namespace VotingSystem.Api.Repositories.Interfaces
+{
+    public interface IVoterRepository
+    {
+        Task<IEnumerable<Voter>> GetAllVotersAsync();
+        Task<Voter?> GetVoterByUserNameAsync(string userName);
+        Task AddVoterAsync(Voter voter);
+        void DeleteVoter(Voter voter);
+        Task<bool> VoterExists(string userName);
+        Task SaveChangesAsync();
+    }
+}
---
+++ VotingSystem/VotingSystem.Api/Repositories/VoterRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VotingSystem.Api.DbContexts;
+using VotingSystem.Api.Entities;
+using VotingSystem.Api.Repositories.Interfaces;
+
+namespace VotingSystem.Api.Repositories
+{
+    public class VoterRepository : IVoterRepository
+    {
+        private readonly VotingSystemContext _context;
+
+        public VoterRepository(VotingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Voter>> GetAllVotersAsync()
+        {
+            return await _context.Voters.OrderBy(o => o.UserName).ToListAsync();
+        }
+
+        public async Task<Voter?> GetVoterByUserNameAsync(string userName)
+        {
+            return await _context.Voters.FirstOrDefaultAsync(o => o.UserName == userName);
+        }
+
+        public async Task AddVoterAsync(Voter voter)
+        {
+            await _context.Voters.AddAsync(voter);
+        }
+
+        public void DeleteVoter(Voter voter)
+        {
+            _context.Voters.Remove(voter);
+        }
+
+        public async Task<bool> VoterExists(string userName)
+        {
+            return await _context.Voters.AnyAsync(o => o.UserName == userName);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
