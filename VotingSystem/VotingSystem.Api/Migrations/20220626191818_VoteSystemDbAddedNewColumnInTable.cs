7b337eb20244bf202c2277f30d6e666a11c950bd
Anna Marija Vrublevska <annamarija.vrublevska@gmail.com>
2022-06-26 22:38:32 +0300

    added new column

---
+++ VotingSystem/VotingSystem.Api/Migrations/20220626191818_VoteSystemDbAddedNewColumnInTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VotingSystem.Api.Migrations
+{
+    public partial class VoteSystemDbAddedNewColumnInTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VoteDate",
+                table: "VoteHistory",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VoteDate",
+                table: "VoteHistory");
+        }
+    }
+}
